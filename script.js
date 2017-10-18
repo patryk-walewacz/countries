ae2a6a033d52c841bd0fd7542a7ee139fdcbabe8
patryk-walewacz <patryk24oln@gmail.com>
2017-10-18 20:06:33 +0200

    #

---
+++ script.js
@@ -0,0 +1,24 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  	var countryName = $('#country-name').val();
+  	if(!countryName.length) countryName = 'Poland';
+
+  	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+
+function showCountriesList(resp) {
+	countriesList.empty();
+	resp.forEach(function(item){
+   		var li = $('<li>').text(item.name).appendTo(countriesList);
+   		$('<span>').text(item.capital).appendTo(li);
+	});
+}
